5b494d9e01befa94742fb6c9464e201794a6dc34
APM <albertopardomarti@gmail.com>
2022-08-17 18:56:02 +0200

    probado en local y visulizando datos con console.log()

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { DataStore } from '@aws-amplify/datastore';
+import { Todo } from './models';
+
+
+async function data() {
+  // Create
+  await DataStore.save(
+    new Todo({
+		"name": "Lorem ipsum dolor sit amet",
+		"description": "Lorem ipsum dolor sit amet"
+	})
+  );
+	
+  // Query
+  const models = await DataStore.query(Todo);
+  console.log(models);
+}
+
+
 function App() {
+
+  data()
+
   return (
     <div className="App">
       <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { Amplify } from "aws-amplify";
 import awsconfig from "./aws-exports";
 
-Amplify.configure(awsconfig);
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+Amplify.configure(awsconfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
